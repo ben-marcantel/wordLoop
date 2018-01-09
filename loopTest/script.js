3732a7052e239447b4022f58ac31236e0db90b0e
ben.marcantel <33232420+ben-marcantel@users.noreply.github.com>
2018-01-08 21:06:42 -0600

    Update script.js

--- loopTest/script.js
+++ loopTest/script.js
@@ -1,6 +1,6 @@
 
 const canvas = document.querySelector("canvas");
-const c =canvas.getContext('2d');
+const c = canvas.getContext('2d');
 
 
 const t = Math.floor((Math.random()*5));
@@ -10,13 +10,11 @@ canvas.height = window.innerHeight;
 
 
    
-function Word(x,y,dx,dy,radius,dRadius,r,dr,g,dg,b,db,radians,velocity){
+function Word(x,y,dx,dy,r,dr,g,dg,b,db,radians,velocity){
     this.x =x;
     this.y =y;
     this.dx = dx;
     this.dy = dy;
-    this.radius = Math.floor((Math.random() * 100) +6);
-    this.dRadius =1;
     this.r = Math.floor((Math.random() * 255) +1);
     this.dr= .25;
     this.g = Math.floor((Math.random() * 255) +1);
@@ -30,17 +28,17 @@ function Word(x,y,dx,dy,radius,dRadius,r,dr,g,dg,b,db,radians,velocity){
         c.beginPath();
         c.moveTo(4,5);
         c.font = "100px Futura";
-        c.strokeText(`financier`,this.x,this.y,)
+        c.strokeText(`so neon`,this.x,this.y,)
         c.strokeStyle='rgb(' + this.r +',' + this.g + ',' + this.b + ')';
         c.stroke();
         c.fillStyle = 'rgb(0,0,0)';
     }
 
     this.update = function(){
-        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+        if (this.x > innerWidth || this.x < 0) {
             this.dx = -this.dx;
         }
-        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+        if (this.y > innerHeight || this.y < 0) {
             this.dy = -this.dy;
         }
         if(this.r > 255 || this.r<1){
@@ -52,16 +50,13 @@ function Word(x,y,dx,dy,radius,dRadius,r,dr,g,dg,b,db,radians,velocity){
         if(this.b > 255 || this.b<1){
             this.db= -this.db;
         }
-        if(this.radius>100|| this.radius<1){
-            this.dRadius = -this.dRadius; 
-        }
+        
         this.radians += this.velocity;
         this.x = x + Math.cos(this.radians)*10*1/Math.sin(this.radians);
         this.y = y + (Math.sin(this.radians)*10*1/Math.cos(this.radians));
         this.r += this.dr;
         this.g += this.dg;
         this.b += this.db;
-        this.radius += this.dRadius;
         this.draw();
     }
 }  
@@ -69,8 +64,6 @@ function Word(x,y,dx,dy,radius,dRadius,r,dr,g,dg,b,db,radians,velocity){
 let wordArray =[];
 
 for (i=0;i<5;i++){
-   let radius = Math.floor((Math.random() * 100) +6);
-   let dRadius =2;
    let x = Math.random() * (innerWidth - radius * 2) + radius;
    let y = Math.random() * (innerHeight - radius * 2) +  radius;
    let dx = 1
@@ -81,7 +74,7 @@ for (i=0;i<5;i++){
    let dg= .25;
    let b = Math.floor((Math.random()*255)+1);
    let db = .25;
-   wordArray.push(new Word(x,y,dx,dy,radius,dRadius,r,dr,g,dg,b,db))
+   wordArray.push(new Word(x,y,dx,dy,r,dr,g,dg,b,db))
 }
 
 
